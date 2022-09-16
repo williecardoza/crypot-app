863fc5ac9352107a902db908091c572049297c97
Willie Cardoza <williecardoza@Willies-MBP.attlocal.net>
2022-09-16 00:29:36 -0700

    changed theme color and set into local storage

---
+++ src/themes.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const darkTheme = {
+  main: "#1F2128",
+  secondary: "#191B1F",
+  third: "#2C2F36",
+  color: "#FFFFFF"
+};
+
+export const lightTheme = {
+  main: "#FCFCFC",
+  secondary: "#FFFFFF",
+  third: "#FCFCFC",
+  color: "#2C2F36"
+};
+
+export const GlobalStyles = createGlobalStyle`
+body {
+   background-color: ${props => props.theme.main};
+   color: ${props => props.theme.color}
+}
+`;
